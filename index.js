771cfbbd85a1ef698fcde00eacdd74828dce0055
Abrar <ahmedabrararian@gmail.com>
2024-12-05 19:48:31 +0600

    Received AddReview Data on the server-side

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post('/review', async(req, res)=> {
+      const notunReview = req.body;
+      console.log(notunReview);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
