272c596b72bb24d2a577eb7ddaa191b753a805ce
Abrar <ahmedabrararian@gmail.com>
2024-12-06 08:02:32 +0600

    Fixed Loading singleData issue by adding a '/'

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ async function run() {
       res.send(result);
     })
 
-    app.get('reviews/:id', async(req, res)=> {
+    app.get('/reviews/:id', async(req, res)=> {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const review = await allReviews.findOne(query);
