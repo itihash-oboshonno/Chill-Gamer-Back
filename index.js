dea3695ca905a54e7edce6ad8d523ea9b2302351
Abrar <ahmedabrararian@gmail.com>
2024-12-07 18:58:49 +0600

    Added delete function for watchlist

--- index.js
+++ index.js
@@ -76,6 +76,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/mywatchlist/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await wishList.deleteOne(query);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
